44fb91c1c70e246be2df12ddb6f6b81ddb8c1aae
Jasmer <jasmer.sanjotra@gmail.com>
2024-04-26 13:04:51 +0700

    test

---
+++ CP Sheets/Maths/A_Most_Unstable_Array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+//author: von_Braun
+#define ll long long
+#define lli long long int
+#define pb push_back
+#define rep(var, start, num) for(ulli var = start; var <start + num; var++)
+#define all(x) x.begin(), x.end()
+#define ulli unsigned long long int
+#define ull unsigned long long
+bool sortbysec(const pair<ll,ll> &a,const pair<ll,ll> &b) { return (a.second < b.second); }
+
+void solve() {
+    ll int n,m;
+    cin>>n>>m;
+    if (n==1) {cout<<0<<endl;}
+    else if (n==2) {cout<<m<<endl;}
+    else {cout<<2*m<<endl;}     
+}
+int main() {
+    //add quotes incase input output file
+    //freopen(input.txt,r,stdin);
+    //freopen(output.txt,w,stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        solve();
+    }
+}
